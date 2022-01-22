39c04ea2e4e96c6930a8dfc6943c466c073c42d2
LaurenDarrimon <laurendarrimon@gmail.com>
2022-01-21 18:37:30 -0800

    pass input responses to write file function and write test README

--- index.js
+++ index.js
@@ -18,12 +18,43 @@ const questions =
         {
           type: 'input',
           message: 'What is the name of this repo?',
-          name: 'location',
+          name: 'fileName',
         },
+        {
+            type: 'input',
+            message: 'Please enter a description of the site.',
+            name: 'description',
+          },
     ];
 
 // Create a function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, response) {
+
+    //object destructuring to declare new constants from user input  
+    const {fullName, gitUser, description} = response;
+
+    
+    
+    
+    console.log(fileName);
+    console.log(fullName);
+    console.log(gitUser);
+    console.log(description);
+    console.log(response);
+
+
+    const markdownText = 
+
+`# ${fullName}
+## ${fileName}
+    
+## Description
+${description}
+`
+
+    fs.writeFile(`${fileName}README.md`, markdownText, (err) =>
+    err ? console.error(err) : console.log('Success!')
+);
   
 
 
@@ -35,6 +66,13 @@ function init() {
     .prompt(questions)
     .then((response) => {
         console.log(response);
+
+        const {fileName} = response;
+
+
+
+        writeToFile(fileName, response)
+
     });
 }
 
